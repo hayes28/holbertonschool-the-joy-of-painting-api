6765ca01f1ac8a1658ecae01df8d3b752b4f3adb
hayes28 <heather.hayes918@gmail.com>
2023-11-28 18:01:37 -0600

    Color data is all cleaned!

--- api/colorsUsed.js
+++ api/colorsUsed.js
@@ -1,56 +1,25 @@
 const fs = require("fs");
-const csv = require("csv-parser");
 
-const colorsUsedFilePath =
-  "/home/hayes/holbertonschool-the-joy-of-painting-api/data_list/The Joy Of Painiting - Colors Used";
-const outputFilePath =
+const colorsFilePath =
   "/home/hayes/holbertonschool-the-joy-of-painting-api/data_list/The_Joy_Of_Painting_Colors.json";
 
-const colorsData = [];
-
-fs.createReadStream(colorsUsedFilePath)
-  .pipe(csv())
-  .on("data", (row) => {
-    const season = row.season.padStart(2, "0");
-    const episode = row.episode.padStart(2, "0");
-    const episodeId = `S${season}E${episode}`;
-
-    const episodeData = {
-      painting_index: row.painting_index.toLowerCase(),
-      img_src: row.img_src.toLowerCase(),
-      title: row.painting_title.toLowerCase(),
-      episode: episodeId,
-      num_colors: row.num_colors.toLowerCase(),
-      youtube_src: row.youtube_src.toLowerCase(),
-      colors: parseColors(row.colors), // Function to parse color names
-    };
-
-    colorsData.push(episodeData);
-  })
-  .on("end", () => {
-    fs.writeFile(
-      outputFilePath,
-      JSON.stringify(colorsData, null, 2),
-      "utf8",
-      (err) => {
-        if (err) {
-          console.error("Error writing file:", err);
-          return;
-        }
-        console.log("JSON file has been saved");
-      }
-    );
-  });
-
-function parseColors(colorString) {
-
-  try {
-    return colorString
-      .match(/\[([^\]]+)\]/)[1]
-      .split(",")
-      .map((color) => color.trim().replace(/\r\n/g, "").toLowerCase());
-  } catch (e) {
-    console.error("Error parsing colors:", e);
-    return [];
-  }
+function parseColors(colorArray) {
+  return colorArray.map((color) =>
+    color
+      .replace(/[\[\]'\\]/g, "") // Remove brackets, quotes, and backslashes
+      .trim()
+      .toLowerCase()
+  );
 }
+
+// Read the colors data from the updated JSON file
+let colorsData = JSON.parse(fs.readFileSync(colorsFilePath, "utf8"));
+
+// Process each entry to fix the colors formatting
+colorsData.forEach((entry) => {
+  entry.colors = parseColors(entry.colors);
+});
+
+// Write the updated data back to the JSON file
+fs.writeFileSync(colorsFilePath, JSON.stringify(colorsData, null, 2), "utf8");
+console.log("Updated colors data saved.");
